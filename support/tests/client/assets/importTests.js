b2f5854371cafe0f4ec6b607515d33b0336b152c
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2015-05-21 11:41:19 -0400

    Completed all simple importer tests

--- support/tests/client/assets/importTests.js
+++ support/tests/client/assets/importTests.js
@@ -1,79 +1,81 @@
-var models = ["usmale.dae"];
-var base = "http://localhost:3000/adl/sandbox/";
-var tests = ['Test import of unoptimized Collada file', "Test import of optimized Collada file"];
+var globalBase = "http://localhost:3000/adl/sandbox/";
 var outArr = [];
+module.exports = function(){ return outArr; };
 
-module.exports = function(){
-	return outArr;
-};
-//		https://3dr.adlnet.gov/api/_3DRAPI.svc/adl:508/Format/json?ID=00-00-00
-//		https://3dr.adlnet.gov/api/rest/adl:508/Metadata/json?id=00-00-00
+var tests = [
+	{ title: "Test import of unoptimized Collada file", model: "usmale.dae"},
+	{ title: "Test import of optimized Collada file", model: "usmale.dae"},
+	{ title: "Test import of 3DR JSON file", model: "house2.json", base: globalBase + "examples/3drjson/"},
+	{ title: "Test import of glTF JSON file", model: "monster.json", base: globalBase + "examples/gltfAsset/"}
+];
 		
 for(var i = 0; i < tests.length; i++){
 	outArr.push({
-		title: tests[i], 
+		title: tests[i].title, 
 		test: 
 			function(i){
 				return function(browser, finished){
-					runAssetTest(browser, finished, i);
+					runAssetTest(browser, finished, tests[i]);
 				}
 			}(i)
 	});
 }
 
-function runAssetTest(browser, finished, i){
+function runAssetTest(browser, finished, test){
 	global.browser = browser;
 	var testUtils = global.testUtils;
 	var outStr = "";
 	var passed = true;
+	var i = tests.indexOf(test);
 	
 	browser.loadBlankScene();
 
-	loadModel("this_shouldnt_exist.dae")
-		.pause(1000)
-		.hasViewNode("this_shouldnt_exist.dae", function(err, exists){
-			passed = passed && !exists;
-			outStr += "Nonexistent model: " + exists + ", expected: false; ";
-		})
-		.getText("#alertify .alertify-message", function(err, msg){
-			var success = msg.toLowerCase().indexOf("error") > -1;
-			passed = passed && success;
-			outStr += "Error message: "+success+", expected: true; ";
-		})
-		.click("#alertify-ok");
+	//do not run nonexistent test when loading glTF JSON (tests[3])
+	if(i != 3){
+		loadModel("this_shouldnt_exist", test.base)
+			.pause(1000)
+			.hasViewNode("this_shouldnt_exist", function(err, exists){
+				passed = passed && !exists;
+				outStr += "Nonexistent model: " + exists + ", expected: false; ";
+			})
+			.getText("#alertify .alertify-message", function(err, msg){
+				var success = msg.toLowerCase().indexOf("error") > -1;
+				passed = passed && success;
+				outStr += "Error message: "+success+", expected: true; ";
+			})
+			.click("#alertify-ok");
+	}
 		
-	loadModel(models[0])		
+	loadModel(test.model, test.base)		
 		.pause(6000).then(function() {
-			testUtils.assertNodeExists(models[0], function(assertStatus, msg){
+			testUtils.assertNodeExists(test.model, function(assertStatus, msg){
 				passed = passed && !!assertStatus;
 				outStr += msg + "; ";
 			});
 		})
-		.hasViewNode(models[0], function(err, exists){
-			
+		.hasViewNode(test.model, function(err, exists){
 			passed = passed && exists;
-			outStr += models[0] + ": " + exists + ", expected: true; ";
+			outStr += test.model + ": " + exists + ", expected: true; ";
+			console.log('test finished');
 			finished(passed, outStr);
 		});
 		
-	function loadModel(model){
-		return browser.nextGUID(model)
+	function loadModel(modelName, base){
+		return browser.nextGUID(modelName)
 			.click("#MenuCreate")
-			.pause(1000)
+			.pause(500)
 			
 			.click("#MenuCreateLoadMeshURL")
 			
 			.waitForExist("#choice" + i, 2000)
 			.click("#choice" + i)
 			
-			.pause(500)
+			.pause(1000)
 			.waitForExist("#alertify-ok", 2000)
 			.click("#alertify-ok")
 			
 			.waitForExist("#alertify-text", 2000)
-			.setValue("#alertify-text", base + model)
-			
-			.pause(500)
+			.setValue("#alertify-text", base ? base + modelName : globalBase + modelName)
 			.click("#alertify-ok");
 	}
 }
