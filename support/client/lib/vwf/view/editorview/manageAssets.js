1718e7ae93738b87834551958047e7b86967c549
Steven Vergenz <steve.vergenz.ctr@adlnet.gov>
2015-05-29 17:25:06 -0400

    Started on angular data fetching

--- support/client/lib/vwf/view/editorview/manageAssets.js
+++ support/client/lib/vwf/view/editorview/manageAssets.js
@@ -1,17 +1,54 @@
-define({
-	initialize: function(){
-		
-		$("<link rel='stylesheet' href='vwf/view/editorview/css/assets.css'/>").appendTo(document.head);
-		$('<div id="manageAssetsContainer"></div>').appendTo(document.body);
-
-		$('#manageAssetsContainer').load('vwf/view/editorview/manageAssets.html',function()
+define(['vwf/view/editorview/lib/angular', 'vwf/view/editorview/lib/angular-resource'], function(angular)
+{
+	return {
+		initialize: function()
 		{
-			$('#manageAssetsDialog').dialog({
-				title: 'Manage Assets',
-				width: 600,
-				height: 600,
-				autoOpen: false
+
+
+
+			var app = angular.module('ManageAssetsDialog', ['ngResource']);
+
+			app.factory('DataManager', ['$resource','$http', function($resource,$http)
+			{
+				var listResource = $resource('/adl/sas/assets/by-meta/all-of?user_name=:userId&isSandboxAsset=true');
+				var data = listResource.get({userId: 'vergenzs'}, function(list)
+				{
+					for(var id in list.matches)
+					{
+						$http.get('/adl/sas/assets/'+id+'/meta/name').success(function(data){
+							list.matches[id].name = data;
+						});;
+					}
+				});
+
+
+				return data;
+			}]);
+
+			app.controller('AssetListController', ['$scope','$rootScope','DataManager', function($scope,$rootScope,DataManager)
+			{
+				$scope.data = DataManager;
+			}]);
+
+
+
+
+
+			// actually insert the html
+			$("<link rel='stylesheet' href='vwf/view/editorview/css/assets.css'/>").appendTo(document.head);
+			$('<div id="manageAssetsContainer"></div>').appendTo(document.body);
+			$('#manageAssetsContainer').load('vwf/view/editorview/manageAssets.html',function()
+			{
+				$('#manageAssetsDialog').dialog({
+					title: 'Manage Assets',
+					width: 600,
+					height: 600,
+					autoOpen: false
+				});
+
+				angular.bootstrap($('#manageAssetsDialog')[0], ['ManageAssetsDialog']);
 			});
-		});
-	}
+		}
+	};
+	
 });
--- support/client/lib/vwf/view/editorview/manageAssetsController.js
+++
@@ -1,5 +0,0 @@
-var app = angular.module('ManageAssets', ['ngResource']);
-
-$(document).ready(function(){
-	angular.bootstrap( $('#manageAssetsDialog')[0], 'ManageAssets' );
-});
