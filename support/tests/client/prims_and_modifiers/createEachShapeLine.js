8833081c9fe4954bb241ed7d50b52a795c9b9774
Andy Creighton <cr8onski@gmail.com>
2015-06-01 17:23:40 -0400

    tests each shape, passes chrome and firefox, no to ie11

--- support/tests/client/prims_and_modifiers/createEachShapeLine.js
+++ support/tests/client/prims_and_modifiers/createEachShapeLine.js
@@ -17,11 +17,96 @@ module.exports = {
 		.waitForVisible('#MenuCreateLine')
 		.click('#MenuCreateLine')
 		
+		//Create Circle from menu
+		.pause(1000)
+		.nextGUID('testCircle')
+		.click('#MenuCreate')
+		.waitForVisible('#MenuShapes')
+		.click('#MenuShapes')
+		.waitForVisible('#MenuCreateCircle')
+		.click('#MenuCreateCircle')
+		
+		//Create Star from menu
+		.pause(1000)
+		.nextGUID('testStar')
+		.click('#MenuCreate')
+		.waitForVisible('#MenuShapes')
+		.click('#MenuShapes')
+		.waitForVisible('#MenuCreateStar')
+		.click('#MenuCreateStar')
+		
+		//Create Rectangle from menu
+		.pause(1000)
+		.nextGUID('testRectangle')
+		.click('#MenuCreate')
+		.waitForVisible('#MenuShapes')
+		.click('#MenuShapes')
+		.waitForVisible('#MenuCreateRectangle')
+		.click('#MenuCreateRectangle')
+		
+		//Create lSection from menu
+		.pause(1000)
+		.nextGUID('testLSection')
+		.click('#MenuCreate')
+		.waitForVisible('#MenuShapes')
+		.click('#MenuShapes')
+		.waitForVisible('#MenuCreateLSection')
+		.click('#MenuCreateLSection')
+		
+		//Create tSection from menu
+		.pause(1000)
+		.nextGUID('testTSection')
+		.click('#MenuCreate')
+		.waitForVisible('#MenuShapes')
+		.click('#MenuShapes')
+		.waitForVisible('#MenuCreateTSection')
+		.click('#MenuCreateTSection')
+		
 		//Verify Line
 		.pause(3000).then(function() {
 			testUtils.assertNodeExists("testLine", function(assertStatus, msg) {
 				passed = passed && !!assertStatus;
 				outStr += "Line exists: " + msg + "; ";
+			});
+		})
+		
+		//Verify Circle
+		.then(function() {
+			testUtils.assertNodeExists("testCircle", function(assertStatus, msg) {
+				passed = passed && !!assertStatus;
+				outStr += "Circle: " + msg + "; ";
+			});
+		})
+
+		//Verify Star
+		.then(function() {
+			testUtils.assertNodeExists("testStar", function(assertStatus, msg) {
+				passed = passed && !!assertStatus;
+				outStr += "Star: " + msg + "; ";
+			});
+		})
+
+		//Verify Rectangle
+		.then(function() {
+			testUtils.assertNodeExists("testRectangle", function(assertStatus, msg) {
+				passed = passed && !!assertStatus;
+				outStr += "Rectangle: " + msg + "; ";
+			});
+		})
+
+		//Verify L-section
+		.then(function() {
+			testUtils.assertNodeExists("testLSection", function(assertStatus, msg) {
+				passed = passed && !!assertStatus;
+				outStr += "L-section: " + msg + "; ";
+			});
+		})
+
+		//Verify T-section
+		.then(function() {
+			testUtils.assertNodeExists("testTSection", function(assertStatus, msg) {
+				passed = passed && !!assertStatus;
+				outStr += "T-section exists: " + msg + "; ";
 				finished(passed, outStr);
 			});
 		});
