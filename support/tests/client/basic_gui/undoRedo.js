53d1e79471297f6a1bd8667d611159e028e62c79
Andy Creighton <cr8onski@gmail.com>
2015-06-10 14:17:48 -0400

    undo redo working

--- support/tests/client/basic_gui/undoRedo.js
+++ support/tests/client/basic_gui/undoRedo.js
@@ -1,10 +1,5 @@
 //Test Undo and Redo
 
-//Not working in chrome, but working fine in firefox, so I'm leaving it alone
-//The message in chrome is that the cone when first tested does not exist, that
-//seems more like a chrome problem than this programs problem, especially since
-//the rest of the test is fine. As of 6/10/2015
-
 module.exports = {
     'Undo and Redo (A13)': function(browser, finished) {
 	    global.browser = browser;
@@ -24,6 +19,7 @@ module.exports = {
 				
 			});
 		})
+		.pause(1000)
 		.nextGUID('Cone')
 		.$click('#MenuCreateConeicon')
 		.pause(3000).then(function() {
@@ -33,29 +29,32 @@ module.exports = {
 				
 			});
 		})
+		.pause(1000)
 		
 		//3 Undo's to make the cone disappear (button)
 		.$click('#MenuUndoicon').$click('#MenuUndoicon').$click('#MenuUndoicon')
 		//Verify the cone is not
 		.pause(3000).then(function() {
-			testUtils.assertNodeExists("testCone", function(assertStatus, msg) {
+			testUtils.assertNodeExists("Cone", function(assertStatus, msg) {
 				passed = passed && !assertStatus;	//Negate to assert non-existence
 				outStr += "Cone Undone: " + msg + "; ";
 				
 			});
 		})
+		.pause(1000)
 
 		//3 Redo's to make the cone reappear and be selected (button)
-		.pause(7000)	//seven seconds seems to work consistently
+		.pause(7000)
 		// .nextGUID('coneRedone')	//not needed after all, nextGUID is restored with redo
 		.$click('#MenuRedoicon').$click('#MenuRedoicon').$click('#MenuRedoicon')
 		//Verify the cone is
-		.pause(3000).then(function() {
-			testUtils.assertNodeExists("testCone", function(assertStatus, msg) {
+		.pause(6000).then(function() {
+			testUtils.assertNodeExists("Cone", function(assertStatus, msg) {
 				passed = passed && !!assertStatus;
 				outStr += "Cone Redone: " + msg + "; ";
 				finished(passed, outStr);
 			});
 		})
+	
 	}
 }
