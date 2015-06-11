48047a81fb08b2a710f6edb4965e31e0b1f95417
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2015-06-11 10:07:14 -0400

    Slightly better modifier stack testing

--- support/tests/client/modifiers/testStack.js
+++ support/tests/client/modifiers/testStack.js
@@ -6,6 +6,7 @@ module.exports = {
 		var outStr = "";
 		var worldId;
 		var oldUUID = "";
+		var secondUUID = "";
 		
 		var nodeName = "testWorldPrim";
 		var modifiers = [{name: "Bend", vwf: ""}, {name: "Twist", vwf: ""}];
@@ -16,6 +17,7 @@ module.exports = {
 			.waitForExist('#preloadGUIBack', 60000)
 			.waitForVisible('#preloadGUIBack', 60000, true)
 			
+			//Create sphere
 			.nextGUID(nodeName)
 			.$click("#MenuCreateSphereicon")
 			.pause(6000).then(function() {
@@ -25,7 +27,7 @@ module.exports = {
 				});
 			})
 			
-			//Create the modifier
+			//Get the original UUID
 			.pause(1000)
 			.getUUID(nodeName, function(err, uuid){
 				if(err || !uuid){
@@ -34,11 +36,14 @@ module.exports = {
 				}
 				else oldUUID = uuid;	
 			})
+			
+			//Create the Bend modifier
 			.then(function(){
 				return addModifier(modifiers[0].name);
 			})			
 			.pause(1000)
-			//Ensure the first modifier exists..
+			
+			//Ensure that the first modifier exists and is a child of the prim..
 			.getChildren(nodeName, function(err, children){
 				var i = 0;
 				if(children && children[0] && children[0].indexOf(modifiers[i].name.toLowerCase()) > -1){
@@ -51,10 +56,14 @@ module.exports = {
 					outStr += modifiers[i].name + " modifier NOT found; ";
 				}
 			})
+			
+			//Adjust the bend amount
 			.then(function(){
 				return browser.setProperty(modifiers[0].vwf, "amount", 1);
 			})
 			.pause(1000)
+			
+			//The prim should now have a new UUID..
 			.getUUID(nodeName, function(err, uuid){
 				if(err || !uuid){
 					passed = false;
@@ -65,19 +74,20 @@ module.exports = {
 					outStr += "Prim UUID not changed after " + modifiers[0].name + "; ";
 				}
 				else{
-					oldUUID = uuid;	
+					secondUUID = uuid;	
 					outStr += "UUID successfully changed after " + modifiers[0].name + "; ";
 				}					
 			})
+			
+			//Create the Twist modifier
 			.then(function(){
 				return addModifier(modifiers[1].name);
 			})
 			.pause(1000)
+			
+			//Ensure that the second modifier exists and is a child of the first modifier
 			.then(function(){
-				debugger;
-				//Ensure that the second modifier is a child of the first modifier
 				return browser.getChildren(modifiers[0].vwf, function(err, children){
-					debugger;
 					var i = 1;
 					if(children && children[0] && children[0].indexOf(modifiers[i].name.toLowerCase()) > -1){
 						
@@ -91,22 +101,25 @@ module.exports = {
 				});
 			})
 			.pause(1000)
+			
+			//Adjust the twist amount
 			.then(function(){
 				return browser.setProperty(modifiers[1].vwf, "amount", 1);
 			})
 			.pause(1000)
+			
+			//The prim should now have a new UUID (again)..
 			.getUUID(nodeName, function(err, uuid){
 				if(err || !uuid){
 					passed = false;
 					outStr += "Prim UUID not found; ";
 				}
-				else if(uuid == oldUUID){
+				else if(uuid == oldUUID || uuid == secondUUID){
 					passed = false;
 					outStr += "Prim UUID not changed after " + modifiers[1].name + "; ";
 				}
 				else{ 
-					oldUUID = uuid;	
-					outStr += "UUID successfully changed after " + modifiers[0].name + "; ";
+					outStr += "UUID successfully changed after " + modifiers[1].name + "; ";
 				}
 
 				finished(passed, outStr);					
@@ -120,7 +133,7 @@ module.exports = {
 				.$click("#MenuModifiers")
 				.pause(500)
 				.$click("#MenuCreate" + modifier)
-				.pause(500)
+				.pause(500);
 		}
     }
 }
