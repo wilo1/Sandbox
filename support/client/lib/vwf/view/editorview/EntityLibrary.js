b713efde49600ba681e81dc03a346c0ff92efd9c
Rob Chadwick <rchadwic@gmail.com>
2015-06-16 09:25:11 -0400

    update EntityLibrary to include model and material types

--- support/client/lib/vwf/view/editorview/EntityLibrary.js
+++ support/client/lib/vwf/view/editorview/EntityLibrary.js
@@ -209,7 +209,7 @@ define(function() {
                     
                         if(!currentDrag) return;
                         var data = currentDrag;
-                        if (currentDrag.type == 'asset') {
+                        if (currentDrag.type == 'asset' || currentDrag.type == 'model') {
                             if (!EntityLibrary.dropPreview) {
                                 EntityLibrary.dropPreview = new THREE.Mesh(new THREE.SphereGeometry(1, 30, 30), EntityLibrary.createPreviewMaterial());
                                 _dScene.add(EntityLibrary.dropPreview, true);
@@ -233,7 +233,7 @@ define(function() {
                                 }
                             }
                         }
-                        if (currentDrag.type == 'material' || currentDrag.type == 'child' || currentDrag.type == 'environment') {
+                        if (currentDrag.type == 'material' || currentDrag.type == 'child' || currentDrag.type == 'environment' || currentDrag.type == 'texture') {
 
                             if (!EntityLibrary.dropPreview) {
                                 EntityLibrary.dropPreview = new  THREE.Object3D();//new THREE.Mesh(new THREE.SphereGeometry(1, 30, 30), EntityLibrary.createPreviewMaterial());
@@ -338,7 +338,7 @@ define(function() {
         }
         this.create = function(data, evt) {
             //if its a 3d file or a node prototype
-            if (data.type == 'asset') {
+            if (data.type == 'asset' || data.type == 'model') {
                 var pos = _Editor.GetInsertPoint(evt ? evt.originalEvent : null);
                 if(data.snap)
                 {
@@ -347,8 +347,7 @@ define(function() {
                    pos[2] = _Editor.SnapTo(pos[2],data.snap); 
                 }
 
-                
-                $.getJSON(data.url, function(proto) {
+                function(proto) {
 
 
                     //very important to clean the node! Might have accidently left a name or id in the libarary
@@ -365,7 +364,6 @@ define(function() {
                     
                     if(data.dropOffset)
                     {
-
                         var dropOffset = new THREE.Matrix4();
                         dropOffset.fromArray(data.dropOffset);
                         var transform = new THREE.Matrix4();
@@ -374,13 +372,28 @@ define(function() {
                         proto.properties.transform = transform.elements;
                     }
 
-					// maintain reference to asset server, if applicable
-					proto.properties.sourceAssetId = data.sourceAssetId;
+                    // maintain reference to asset server, if applicable
+                    proto.properties.sourceAssetId = data.sourceAssetId;
                    
                     _Editor.createChild('index-vwf', newname, proto);
                     _Editor.SelectOnNextCreate([newname]);
 
-                })
+                }
+                if (data.type == 'asset')
+                    $.getJSON(data.url, createProto);
+                if (data.type == 'model') 
+                {
+                    var proto = {
+                        "extends": "asset.vwf",
+                        "source": data.url,
+                        "type": data.type,
+                        "properties":
+                        {
+                            "sourceAssetId" : data.sourceAssetId
+                        }
+                    }
+                    createProto(proto);
+                }
             }
             if (data.type == 'child') {
 
@@ -418,7 +431,58 @@ define(function() {
                         _PrimitiveEditor.setProperty(ID, 'materialDef', proto);
                     })
                 }
+            }
+            if (data.type == 'texture') {
 
+                var materialDef ={
+                "shininess": 15,
+                "alpha": 1,
+                "ambient": {
+                    "r": 1,
+                    "g": 1,
+                    "b": 1
+                },
+                "color": {
+                    "r": 1,
+                    "g": 1,
+                    "b": 1,
+                    "a": 1
+                },
+                "emit": {
+                    "r": 0,
+                    "g": 0,
+                    "b": 0
+                },
+                "reflect": 0.8,
+                "shadeless": false,
+                "shadow": true,
+                "specularColor": {
+                    "r": 0.5773502691896258,
+                    "g": 0.5773502691896258,
+                    "b": 0.5773502691896258
+                },
+                "specularLevel": 1,
+                "sourceAssetId": data.sourceAssetId,
+                "layers": [{
+                        "alpha": 1,
+                        "blendMode": 0,
+                        "mapInput": 0,
+                        "mapTo": 1,
+                        "offsetx": 0,
+                        "offsety": 0,
+                        "rot": 0,
+                        "scalex": 1,
+                        "scaley": 1,
+                        "src": data.url
+                    }
+                ]
+            },
+
+                var ID = EntityLibrary.GetPick(evt);
+                if (ID) {
+                    proto.sourceAssetId = data.sourceAssetId;
+                    _PrimitiveEditor.setProperty(ID, 'materialDef', proto);
+                }
             }
             if (data.type == 'environment') {
                 $.getJSON(data.url, function(proto) {
