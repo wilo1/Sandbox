519dc26a00a0fcd908e382b04721ec698015636c
Mick Muzac <mick.muzac.ctr@adlnet.gov>
2015-07-06 11:08:01 -0400

    World does not prompt for compatibility test if norender

--- support/client/lib/load.js
+++ support/client/lib/load.js
@@ -82,9 +82,10 @@ if (!window.jQuery) {
 function promptTest(boot)
 {
 
-       
+		var noRender = window.location.search && window.location.search.indexOf("norender=true") > -1;
         var settings = localStorage['sandboxPreferences'] && JSON.parse(window.localStorage['sandboxPreferences'])
-        if(!settings || !settings.compatability.satisfied)
+		
+        if((!settings || !settings.compatability.satisfied) && !noRender)
         {
             alertify.confirm("It looks like you haven't been here before. It's best if you take the compatability test first. Would you like to test your browser now?",
                 function(ok)
